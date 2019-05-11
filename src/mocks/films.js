6bac0318ecd15b59963e1aee31b33f2421c668d9
Nikolai <rus.nick@mail.ru>
2019-05-11 13:17:42 +0300

    Add films mock

---
+++ src/mocks/films.js
@@ -0,0 +1,40 @@
+
+export default [
+  {
+    id: `480d2236-6c7d-4fd3-a225-80c66710a71f`,
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    img: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+  }, {
+    id: `505f451a-0b69-47ed-96c6-66d5525d5a5e`,
+    title: `Bohemian Rhapsody`,
+    img: `img/bohemian-rhapsody.jpg`,
+  }, {
+    id: `1fa4cb3b-6aa4-4961-b8eb-be3a92e93af7`,
+    title: `Macbeth`,
+    img: `img/macbeth.jpg`,
+  }, {
+    id: `b15a1da5-8142-4d2a-b567-26599e333988`,
+    title: `Aviator`,
+    img: `img/aviator.jpg`,
+  }, {
+    id: `bf8d20be-4ffc-4759-a7f7-cf5f6f0dc67b`,
+    title: `We need to talk about Kevin`,
+    img: `img/we-need-to-talk-about-kevin.jpg`,
+  }, {
+    id: `466623a4-c3ea-44cc-b067-3284fb8012c7`,
+    title: `What We Do in the Shadows`,
+    img: `img/what-we-do-in-the-shadows.jpg`,
+  }, {
+    id: `474d9e75-3dfe-4e82-a9f0-30fc54e20101`,
+    title: `Revenant`,
+    img: `img/revenant.jpg`,
+  }, {
+    id: `abdc7c6e-9a4f-4497-98fb-331e8101101e`,
+    title: `Johnny English`,
+    img: `img/johnny-english.jpg`,
+  }, {
+    id: `c0ae31ad-7135-430c-8213-90acb78ad1f7`,
+    title: `Shutter Island`,
+    img: `img/shutter-island.jpg`,
+  },
+];
