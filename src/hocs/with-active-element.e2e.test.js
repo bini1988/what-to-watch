8edede37357696cc374b762352ad77f514503ee9
Nikolai <rus.nick@mail.ru>
2019-05-31 20:13:34 +0300

    Split withActiveElement hoc test cases

--- src/hocs/with-active-element.e2e.test.js
+++ src/hocs/with-active-element.e2e.test.js
@@ -12,7 +12,7 @@ function MockComponent() {
 }
 
 describe(`withActiveElement`, () => {
-  it(`should change activeElement prop with set and reset handlers`, () => {
+  it(`should set activeElement prop with setActiveElement handler`, () => {
     const WrappedMockComponent = withActiveElement(MockComponent);
     const wrapper = mount(
         <WrappedMockComponent/>
@@ -28,10 +28,27 @@ describe(`withActiveElement`, () => {
     wrapper.update();
     component = wrapper.find(MockComponent);
     expect(component.prop(`activeElement`)).toEqual(element);
+  });
+  it(`should reset activeElement prop with resetActiveElement handler`, () => {
+    const WrappedMockComponent = withActiveElement(MockComponent);
+    const wrapper = mount(
+        <WrappedMockComponent/>
+    );
 
+    const element = {data: `data`};
+    let component = null;
+
+    component = wrapper.find(MockComponent);
+    expect(component.prop(`activeElement`)).toBeUndefined();
+
+    component.prop(`setActiveElement`)(element);
+    wrapper.update();
+
+    component = wrapper.find(MockComponent);
     component.prop(`resetActiveElement`)();
     wrapper.update();
     component = wrapper.find(MockComponent);
+
     expect(component.prop(`activeElement`)).toBeUndefined();
   });
 });
