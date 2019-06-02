c10ee89485bdd03f3cda05614a4bdef1cceebe28
Nikolai <rus.nick@mail.ru>
2019-06-02 22:26:36 +0300

    Add movies group mock

---
+++ src/mocks/movies-groups.js
@@ -0,0 +1,14 @@
+
+export default {
+  "All Genres": [
+    {
+      id: 11,
+      title: `Fantastic Beasts: The Crimes of Grindelwald`,
+      img: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    }, {
+      id: 22,
+      title: `Bohemian Rhapsody`,
+      img: `img/bohemian-rhapsody.jpg`,
+    }
+  ]
+};
