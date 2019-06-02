dc6f970ff1122285cfb773a1406a2d9620d93bb9
Nikolai <rus.nick@mail.ru>
2019-06-02 14:05:25 +0300

    PageFooter coderef

--- src/components/page-footer/page-footer.jsx
+++ src/components/page-footer/page-footer.jsx
@@ -1,20 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
+import WTWLogo from "../wtw-logo/wtw-logo.jsx";
 
-function PageFooter() {
+function PageFooter({children}) {
   return (
     <footer className="page-footer">
-      <div className="logo">
-        <a className="logo__link logo__link--light">
-          <span className="logo__letter logo__letter--1">{`W`}</span>
-          <span className="logo__letter logo__letter--2">{`T`}</span>
-          <span className="logo__letter logo__letter--3">{`W`}</span>
-        </a>
-      </div>
+      <WTWLogo mode="light"/>
       <div className="copyright">
         <p>{`© 2019 What to watch Ltd.`}</p>
       </div>
+      {children}
     </footer>
   );
 }
+
+PageFooter.propTypes = {
+  /** Вложенные элементы */
+  children: PropTypes.any,
+};
 
 export default PageFooter;
