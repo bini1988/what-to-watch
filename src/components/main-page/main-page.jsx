53869b97de263111d5d72e77d125be0b333c5c18
Nikolai <rus.nick@mail.ru>
2019-06-04 21:06:57 +0300

    Add MainPage SignIn logic

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {changeMoviesActiveGenre} from "../../reducer/catalog/catalog";
 import {getMoviesByGenres, getActiveGenre} from "../../reducer/catalog/selectors";
+import {requireAuthorization} from "../../reducer/user/user";
+import {getUserProfile} from "../../reducer/user/selectors";
 
 import MovieCard from "../movie-card/movie-card";
 import PageHeader from "../page-header/page-header";
@@ -11,17 +13,21 @@ import UserBlock from "../user-block/user-block";
 import MoviesCatalog from "../movies-catalog/movies-catalog";
 
 import card from "../../mocks/movie-card";
-import user from "../../mocks/user";
 
 function MainPage(props) {
-  const {moviesGenreGroups, activeGenre, onGenreChange} = props;
+  const {user, moviesGenreGroups, activeGenre, onGenreChange, onSignIn} = props;
 
   return (
     <React.Fragment>
       <MovieCard card={card}>
         <h1 className="visually-hidden">{`WTW`}</h1>
         <PageHeader className="movie-card__head">
-          <UserBlock user={user}/>
+          <UserBlock
+            user={user}
+            onSignIn={(event) => {
+              event.preventDefault();
+              onSignIn();
+            }}/>
         </PageHeader>
       </MovieCard>
       <div className="page-content">
@@ -36,12 +42,16 @@ function MainPage(props) {
 }
 
 MainPage.propTypes = {
+  /** Данные пользователя */
+  user: UserBlock.propTypes.user,
   /** Список отображаемых фильмов группированных по жанрам */
   moviesGenreGroups: MoviesCatalog.propTypes.moviesGenreGroups,
   /** Активный жанр */
   activeGenre: MoviesCatalog.propTypes.activeGenre,
   /** Изменить фильтр списка фильмов по жанру */
   onGenreChange: MoviesCatalog.propTypes.onGenreChange,
+  /** Авторизовать пользователя */
+  onSignIn: PropTypes.func,
   /** Вложенные элементы */
   children: PropTypes.any,
 };
@@ -50,10 +60,12 @@ const mapStateToProps = (state) => {
   return {
     moviesGenreGroups: getMoviesByGenres(state),
     activeGenre: getActiveGenre(state),
+    user: getUserProfile(state),
   };
 };
 const mapDispatchToProps = {
   onGenreChange: changeMoviesActiveGenre,
+  onSignIn: requireAuthorization,
 };
 
 export {MainPage};
