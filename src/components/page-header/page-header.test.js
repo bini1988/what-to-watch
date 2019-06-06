0a6ab15384f96cff4b9bb841fac50ce6aee3da21
Nikolai <rus.nick@mail.ru>
2019-06-06 21:57:02 +0300

    Update page footer & header tests

--- src/components/page-header/page-header.test.js
+++ src/components/page-header/page-header.test.js
@@ -1,8 +1,14 @@
 import React from "react";
+import {MemoryRouter} from "react-router-dom";
 import renderer from "react-test-renderer";
 import PageHeader from "./page-header.jsx";
 
 it(`PageHeader correctly renders default markup`, () => {
-  const tree = renderer.create(<PageHeader/>).toJSON();
+  const tree = renderer.create(
+      <MemoryRouter
+        initialEntries = {[`/`]}>
+        <PageHeader/>
+      </MemoryRouter>
+  ).toJSON();
   expect(tree).toMatchSnapshot();
 });
