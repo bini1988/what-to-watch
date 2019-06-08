75cd79a7b39fc744fcba722de42c5705ef0698cc
Nikolai <rus.nick@mail.ru>
2019-06-08 21:56:43 +0300

    Update reviews reducer

--- src/reducer/reviews/reviews.js
+++ src/reducer/reviews/reviews.js
@@ -1,64 +1,62 @@
 
 export const ActionTypes = {
-  ADD_REVIEWS: `ADD_REVIEWS`,
+  STORE_REVIEWS: `STORE_REVIEWS`,
 };
 
-/**
- * Сохранить список отзывов к фильму
- * @param {number} id ID фильма
- * @param {Object[]} reviews Список отзывов
- * @return {Object}
- */
-export const addReviews = (id, reviews) => {
-  return {type: ActionTypes.ADD_REVIEWS, meta: {id}, payload: reviews};
+export const ActionCreator = {
+  /**
+   * Сохранить список отзывов к фильму
+   * @param {number} id ID фильма
+   * @param {Object[]} reviews Список отзывов
+   * @return {Object}
+   */
+  storeReviews: (id, reviews) => {
+    return {type: ActionTypes.STORE_REVIEWS, meta: {id}, payload: reviews};
+  },
 };
 
-/**
- * Получить список отзывов к фильму
- * @param {number} id ID фильма
- * @return {Promise}
- */
-export const fetchMovieReviews = (id) => {
-  return (dispath, getState, api) => {
-    return api.fetchMovieReviews(id)
-      .then((reviews) => {
-        dispath(addReviews(id, reviews));
-      });
-  };
+export const Operation = {
+  /**
+   * Получить список отзывов к фильму
+   * @param {number} id ID фильма
+   * @return {Promise}
+   */
+  fetchMovieReviews: (id) => {
+    return (dispath, getState, api) => {
+      return api.fetchMovieReviews(id)
+        .then((reviews) => {
+          dispath(ActionCreator.storeReviews(id, reviews));
+        });
+    };
+  },
+  /**
+   * Опубликовать отзыв к фильму
+   * @param {number} id ID фильма
+   * @param {Object} review Отзыв к фильму
+   * @param {number} review.rating Оценка фильма
+   * @param {string} review.comment Текст отзыва
+   * @return {Promise}
+   */
+  submitMovieReview: (id, review) => {
+    return (dispath, getState, api) => {
+      return api.submitMovieReview(id, review)
+        .then((reviews) => {
+          dispath(ActionCreator.storeReviews(id, reviews));
+        });
+    };
+  },
 };
 
-/**
- * Опубликовать отзыв к фильму
- * @param {number} id ID фильма
- * @param {Object} review Отзыв к фильму
- * @param {number} review.rating Оценка фильма
- * @param {string} review.comment Текст отзыва
- * @return {Promise}
- */
-export const submitMovieReview = (id, review) => {
-  return (dispath, getState, api) => {
-    return api.submitMovieReview(id, review)
-      .then((reviews) => {
-        dispath(addReviews(id, reviews));
-      });
-  };
-};
 
 export const initialState = {
-  /** Map-объект отзывов */
-  items: {},
+  /** Массив отзывов */
+  items: [],
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case ActionTypes.ADD_REVIEWS:
-      return {
-        ...state,
-        items: {
-          ...state.items,
-          ...action.payload,
-        },
-      };
+    case ActionTypes.STORE_REVIEWS:
+      return {...state, items: action.payload};
     default:
       return state;
   }
