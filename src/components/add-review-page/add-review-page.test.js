217709480bdc111754eeb7d2c4879c9d6e770936
Nikolai <rus.nick@mail.ru>
2019-06-09 21:07:37 +0300

    Update AddReviePage component

--- src/components/add-review-page/add-review-page.test.js
+++ src/components/add-review-page/add-review-page.test.js
@@ -1,17 +1,22 @@
 import React from "react";
 import {MemoryRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
 import renderer from "react-test-renderer";
 import {AddReviewPage} from "./add-review-page";
-import user from "../../mocks/user";
+
+const initialState = {};
+const mockStore = configureStore();
 
 it(`AddReviewPage correctly renders default markup`, () => {
   const tree = renderer.create(
-      <MemoryRouter
-        initialEntries = {[`/`]}>
-        <AddReviewPage
-          user={user}
-          onSubmit={() => {}}/>
-      </MemoryRouter>
+      <Provider store={mockStore(initialState)}>
+        <MemoryRouter
+          initialEntries = {[`/`]}>
+          <AddReviewPage
+            onReviewSubmit={() => {}}/>
+        </MemoryRouter>
+      </Provider>
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
