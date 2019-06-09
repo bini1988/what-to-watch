a52c36d9e632e428dd5ee3beb6fdcabeb1e06d62
Nikolai <rus.nick@mail.ru>
2019-06-09 20:52:49 +0300

    Add MovieNav active element

--- src/components/movie-nav/movie-nav.test.js
+++ src/components/movie-nav/movie-nav.test.js
@@ -5,7 +5,7 @@ import MovieNav from "./movie-nav";
 it(`MovieNav correctly renders default markup`, () => {
   const tree = renderer.create(
       <MovieNav>
-        <MovieNav.Item label="item-1" href="href-1"/>
+        <MovieNav.Item label="item-1" href="href-1" active/>
         <MovieNav.Item label="item-2" href="href-2"/>
         <MovieNav.Item label="item-3" href="href-3"/>
       </MovieNav>
