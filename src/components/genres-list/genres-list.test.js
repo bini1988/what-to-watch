46afa728815e7fbfb8bd7fb3f86b003a3ab0098e
Nikolai <rus.nick@mail.ru>
2019-06-09 11:27:10 +0300

    Add GenresList empty genres list check

--- src/components/genres-list/genres-list.test.js
+++ src/components/genres-list/genres-list.test.js
@@ -3,11 +3,23 @@ import renderer from "react-test-renderer";
 import GenresList from "./genres-list.jsx";
 
 it(`GenresList correctly renders default markup`, () => {
-  const moviesMock = [`Drama`, `Comedy`];
+  const genres = [`Drama`, `Comedy`];
   const tree = renderer
     .create(
         <GenresList
-          genres={moviesMock}
+          genres={genres}
+          activeGenre="All genres"
+          onGenreChange={() => {}}/>
+    ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it(`GenresList correctly renders empty genres list`, () => {
+  const tree = renderer
+    .create(
+        <GenresList
+          genres={[]}
           activeGenre="All genres"
           onGenreChange={() => {}}/>
     ).toJSON();
