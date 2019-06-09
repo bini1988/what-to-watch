0299efeda1bff0c04d371da53445419396e075dd
Nikolai <rus.nick@mail.ru>
2019-06-09 11:09:55 +0300

    Update mocks

--- src/mocks/movie-card.js
+++ src/mocks/movie-card.js
@@ -1,13 +1,26 @@
 
 export default {
   id: 1,
-  title: `Beach`,
-  genre: `Adventure`,
-  year: 2000,
+  title: `A Star Is Born`,
+  genre: `Drama`,
+  year: 2018,
+  director: `Bradley Cooper`,
+  description: `A musician helps a young singer find fame as age and alcoholism send his own career into a downward spiral.`,
+  starring: [
+    `Lady Gaga`,
+    `Bradley Cooper`,
+    `Sam Elliott`
+  ],
+  duration: `2h 16m`,
   images: {
-    preview: `https://es31-server.appspot.com/wtw/static/film/preview/beach.jpg`,
-    poster: `https://es31-server.appspot.com/wtw/static/film/poster/beach.jpg`,
-    background: `https://es31-server.appspot.com/wtw/static/film/background/beach.jpg`
+    preview: `https://es31-server.appspot.com/wtw/static/film/preview/A_Star_Is_Born.jpg`,
+    poster: `https://es31-server.appspot.com/wtw/static/film/poster/A_Star_Is_Born.jpg`,
+    background: `https://es31-server.appspot.com/wtw/static/film/background/A_Star_is_Born.jpg`
   },
-  trailer: `http://media.xiph.org/mango/tears_of_steel_1080p.webm`
+  trailer: `http://media.xiph.org/mango/tears_of_steel_1080p.webm`,
+  rating: {
+    score: 7.8,
+    level: `Good`,
+    count: 244484
+  }
 };
