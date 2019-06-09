a40970e37861afc9925281bbd0ee93b1b198367c
Nikolai <rus.nick@mail.ru>
2019-06-09 14:45:34 +0300

    Update MovieCard model & mocks

--- src/mocks/movie-card.js
+++ src/mocks/movie-card.js
@@ -1,26 +1,52 @@
 
+export const MovieCardData = {
+  "name": `War of the Worlds`,
+  "poster_image": `https://es31-server.appspot.com/wtw/static/film/poster/War_of_the_Worlds.jpg`,
+  "preview_image": `https://es31-server.appspot.com/wtw/static/film/preview/war-of-the-worlds.jpg`,
+  "background_image": `https://es31-server.appspot.com/wtw/static/film/background/War_of_the_Worlds.jpg`,
+  "background_color": `#9B7E61`,
+  "description": `As Earth is invaded by alien tripod fighting machines, one family fights for survival.`,
+  "rating": 6.5,
+  "scores_count": 386834,
+  "director": `Steven Spielberg`,
+  "starring": [
+    `Tom Cruise`,
+    `Dakota Fanning`,
+    `Tim Robbin`
+  ],
+  "run_time": 116,
+  "genre": `Adventure`,
+  "released": 2005,
+  "id": 1,
+  "is_favorite": false,
+  "video_link": `http://media.xiph.org/mango/tears_of_steel_1080p.webm`,
+  "preview_video_link": `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp`,
+};
+
 export default {
   id: 1,
-  title: `A Star Is Born`,
-  genre: `Drama`,
-  year: 2018,
-  director: `Bradley Cooper`,
-  description: `A musician helps a young singer find fame as age and alcoholism send his own career into a downward spiral.`,
+  title: `War of the Worlds`,
+  genre: `Adventure`,
+  year: 2005,
+  director: `Steven Spielberg`,
+  description: `As Earth is invaded by alien tripod fighting machines, one family fights for survival.`,
   starring: [
-    `Lady Gaga`,
-    `Bradley Cooper`,
-    `Sam Elliott`
+    `Tom Cruise`,
+    `Dakota Fanning`,
+    `Tim Robbin`
   ],
-  duration: `2h 16m`,
+  duration: `1h 56m`,
+  isInList: false,
   images: {
-    preview: `https://es31-server.appspot.com/wtw/static/film/preview/A_Star_Is_Born.jpg`,
-    poster: `https://es31-server.appspot.com/wtw/static/film/poster/A_Star_Is_Born.jpg`,
-    background: `https://es31-server.appspot.com/wtw/static/film/background/A_Star_is_Born.jpg`
+    preview: `https://es31-server.appspot.com/wtw/static/film/preview/war-of-the-worlds.jpg`,
+    poster: `https://es31-server.appspot.com/wtw/static/film/poster/War_of_the_Worlds.jpg`,
+    background: `https://es31-server.appspot.com/wtw/static/film/background/War_of_the_Worlds.jpg`,
+    backgroundColor: `#9B7E61`,
   },
   trailer: `http://media.xiph.org/mango/tears_of_steel_1080p.webm`,
   rating: {
-    score: 7.8,
+    count: 386834,
     level: `Good`,
-    count: 244484
-  }
+    score: 6.5,
+  },
 };
