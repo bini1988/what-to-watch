1867494521cf092f21961c595c0379ff0868c40e
Nikolai <rus.nick@mail.ru>
2019-06-09 11:09:30 +0300

    Coder of MainPage component

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import {connect} from "react-redux";
 
 import {Operation, ActionCreator} from "../../reducer/movies/movies";
-import {getPromoMovie, getMoviesByGenres, getActiveGenre} from "../../reducer/movies/selectors";
+import {getPromoMovie, getMoviesByGenre, getMoviesGenres, getActiveGenre} from "../../reducer/movies/selectors";
 
 import MovieCard from "../movie-card/movie-card";
 import PageHeader from "../page-header/page-header";
@@ -21,13 +21,13 @@ class MainPage extends PureComponent {
   }
 
   render() {
-    const {promoMovieCard = {}, moviesGenreGroups, activeGenre, onGenreChange} = this.props;
-    const {title, genre, year, images = {}} = promoMovieCard;
+    const {promoMovie = {}, movies, moviesGenres, activeGenre, onGenreChange} = this.props;
+    const {title, genre, year, images = {}} = promoMovie;
 
     return (
       <React.Fragment>
         <MovieCard
-          card={promoMovieCard}
+          card={promoMovie}
           renderHeader={({className}) => (
             <PageHeader className={className}>
               <PageTitle hidden={true}>{`WTW`}</PageTitle>
@@ -49,7 +49,8 @@ class MainPage extends PureComponent {
         </MovieCard>
         <div className="page-content">
           <MoviesCatalog
-            moviesGenreGroups={moviesGenreGroups}
+            movies={movies}
+            moviesGenres={moviesGenres}
             activeGenre={activeGenre}
             onGenreChange={onGenreChange}/>
           <PageFooter/>
@@ -61,10 +62,12 @@ class MainPage extends PureComponent {
 
 MainPage.propTypes = {
   /** Текущий промо фильм */
-  promoMovieCard: PropTypes.object,
-  /** Список отображаемых фильмов группированных по жанрам */
-  moviesGenreGroups: MoviesCatalog.propTypes.moviesGenreGroups,
-  /** Активный жанр */
+  promoMovie: PropTypes.object,
+  /** Список отображаемых фильмов */
+  movies: MoviesCatalog.propTypes.movies,
+  /** Список отображаемых жанров фильмов */
+  moviesGenres: MoviesCatalog.propTypes.moviesGenres,
+  /** Активный жанр фильмов */
   activeGenre: MoviesCatalog.propTypes.activeGenre,
   /** Изменить фильтр списка фильмов по жанру */
   onGenreChange: MoviesCatalog.propTypes.onGenreChange,
@@ -77,16 +80,19 @@ MainPage.propTypes = {
 };
 
 const mapStateToProps = (state) => {
+  const activeGenre = getActiveGenre(state);
+
   return {
-    moviesGenreGroups: getMoviesByGenres(state),
-    activeGenre: getActiveGenre(state),
-    promoMovieCard: getPromoMovie(state),
+    promoMovie: getPromoMovie(state),
+    movies: getMoviesByGenre(state, activeGenre),
+    moviesGenres: getMoviesGenres(state),
+    activeGenre,
   };
 };
 const mapDispatchToProps = {
-  onGenreChange: ActionCreator.changeActiveGenre,
   fetchMovies: Operation.fetchMovies,
   fetchPromoMovie: Operation.fetchPromoMovie,
+  onGenreChange: ActionCreator.changeActiveGenre,
 };
 
 export {MainPage};
--- src/components/main-page/main-page.test.js
+++ src/components/main-page/main-page.test.js
@@ -3,14 +3,16 @@ import {MemoryRouter} from "react-router-dom";
 import renderer from "react-test-renderer";
 import {MainPage} from "./main-page";
 import genreGroupsMock from "../../mocks/movies-groups";
+import promoMovie from "../../mocks/movie-card";
 
 it(`MainPage correctly renders default markup`, () => {
   const tree = renderer.create(
       <MemoryRouter
         initialEntries = {[`/`]}>
         <MainPage
-          moviesGenreGroups={genreGroupsMock}
-          activeGenre="All Genres"
+          promoMovie={promoMovie}
+          movies={genreGroupsMock[`All Genres`]}
+          moviesGenres={Object.keys(genreGroupsMock)}
           onGenreChange={() => {}}
           fetchMovies={() => {}}
           fetchPromoMovie={() => {}}
