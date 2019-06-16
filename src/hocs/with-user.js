3787936ae8f71ce032a72b92edff0433fa29266d
Nikolai <rus.nick@mail.ru>
2019-06-16 11:18:06 +0300

    Coderef of UserBlock component

--- src/hocs/with-user.js
+++
@@ -1,9 +0,0 @@
-import {connect} from "react-redux";
-import {getUserData} from "../reducer/user/selectors";
-
-const mapStateToProps = (state) => {
-  return {user: getUserData(state)};
-};
-
-export default connect(mapStateToProps, {});
-
