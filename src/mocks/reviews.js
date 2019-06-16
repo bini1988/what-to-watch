54684e85c0c16975651fdf1874b5dddc3b4704c9
Nikolai <rus.nick@mail.ru>
2019-06-16 18:55:51 +0300

    Update reviews mocks

--- src/mocks/reviews.js
+++ src/mocks/reviews.js
@@ -3,48 +3,48 @@ export default [
   {
     id: 1,
     author: `Kate Muir`,
-    rating: 8.9,
+    rating: 1.5,
     text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director's funniest and most exquisitely designed movies in years.`,
     date: `2019-05-08T14:13:56.569Z`
   }, {
     id: 2,
     author: `Mollie`,
-    rating: 3,
+    rating: 2.0,
     text: `It was well acted, directed, and the music was good. But the story is yawn. Not trying to rip anybody but I checked my watch a dozen times during this movie.`,
     datetime: `2019-06-14T06:39:07.535Z`,
     date: `2019-06-14T06:39:07.535Z`
   }, {
     id: 3,
     author: `Mollie`,
-    rating: 4.5,
+    rating: 2.5,
     text: `I love this movie. This film is a milestone in cinematography. Great Immersive camera-work. This film is an experience and i has already seen it 4 times and I only see more quality of the film.`,
     datetime: `2019-05-27T06:39:07.535Z`,
     date: `2019-05-27T06:39:07.535Z`
   }, {
     id: 4,
     author: `Mollie`,
-    rating: 2,
+    rating: 3.0,
     text: `I love this movie. This film is a milestone in cinematography. Great Immersive camera-work. This film is an experience and i has already seen it 4 times and I only see more quality of the film.`,
     datetime: `2019-05-27T06:39:07.535Z`,
     date: `2019-05-27T06:39:07.535Z`
   }, {
     id: 5,
     author: `Mollie`,
-    rating: 3,
+    rating: 3.5,
     text: `It was well acted, directed, and the music was good. But the story is yawn. Not trying to rip anybody but I checked my watch a dozen times during this movie.`,
     datetime: `2019-06-14T06:39:07.535Z`,
     date: `2019-06-14T06:39:07.535Z`
   }, {
     id: 6,
     author: `Mollie`,
-    rating: 5.0,
+    rating: 4.0,
     text: `I love this movie. This film is a milestone in cinematography. Great Immersive camera-work. This film is an experience and i has already seen it 4 times and I only see more quality of the film.`,
     datetime: `2019-05-27T06:39:07.535Z`,
     date: `2019-05-27T06:39:07.535Z`
   }, {
     id: 7,
     author: `Mollie`,
-    rating: 2.5,
+    rating: 5.0,
     text: `I love this movie. This film is a milestone in cinematography. Great Immersive camera-work. This film is an experience and i has already seen it 4 times and I only see more quality of the film.`,
     datetime: `2019-05-27T06:39:07.535Z`,
     date: `2019-05-27T06:39:07.535Z`
