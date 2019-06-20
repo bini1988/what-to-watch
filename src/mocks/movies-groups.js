4f25aebba33b2a551fd89768a437e161da4d8ce2
Nikolai Karastelev <n.karastelev@credinform.ru>
2019-06-20 20:47:46 +0300

    Update movies group mock

--- src/mocks/movies-groups.js
+++ src/mocks/movies-groups.js
@@ -1,7 +1,7 @@
 import movies from "./movies";
 
 export default {
-  "All Genres": movies,
+  "All genres": movies,
   "Action": [
     movies[0],
     movies[2],
@@ -14,5 +14,22 @@ export default {
   ],
   "Fantasy": [
     movies[4],
+  ],
+};
+
+export const moviesIdsByGenres = {
+  "All genres": movies.map((it) => it.id),
+  "Action": [
+    movies[0].id,
+    movies[2].id,
+  ],
+  "Crime": [
+    movies[1].id,
+  ],
+  "Comedy": [
+    movies[3].id,
+  ],
+  "Fantasy": [
+    movies[4].id,
   ],
 };
