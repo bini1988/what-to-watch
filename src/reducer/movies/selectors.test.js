cc60d40f8cad1a017f8df1c4798cd122d98a9619
Nikolai Karastelev <n.karastelev@credinform.ru>
2019-06-21 18:57:18 +0300

    Add movies selectors tests

--- src/reducer/movies/selectors.test.js
+++ src/reducer/movies/selectors.test.js
@@ -1,10 +1,11 @@
 import NameSpace from "../name-spaces";
 import movieCard from "../../mocks/movie-card";
-import {getMovies, getMoviesItems, getMovieById} from "./selectors";
+import {getMovies, getMoviesItems, getMovieById, getMovieWithReviewsById, getPromoMovie, getMoviesList, getMyListMovies} from "./selectors";
 import {initialState} from "./movies";
 
 const mockMovies = {
   ...initialState,
+  promoMovieId: movieCard.id,
   itemsIds: [movieCard.id],
   items: {
     [movieCard.id]: movieCard,
@@ -26,5 +27,52 @@ describe(`Movies Selectors`, () => {
   it(`should return movie by id`, () => {
     expect(getMovieById(mockStore, movieCard.id))
       .toEqual(movieCard);
+  });
+  it(`should return movie with review by id`, () => {
+
+  });
+  it(`should return promo movie`, () => {
+    expect(getPromoMovie(mockStore))
+      .toEqual(movieCard);
+  });
+  it(`should return movies list`, () => {
+    const mockMoviesStore = {
+      [NameSpace.Movies]: {
+        ...initialState,
+        itemsIds: [`1`, `2`, `3`],
+        items: {
+          [`1`]: {...movieCard, id: `1`},
+          [`2`]: {...movieCard, id: `2`},
+          [`3`]: {...movieCard, id: `3`},
+        },
+      },
+    };
+
+    expect(getMoviesList(mockMoviesStore))
+      .toEqual([
+        {...movieCard, id: `1`},
+        {...movieCard, id: `2`},
+        {...movieCard, id: `3`},
+      ]);
+  });
+  it(`should return my movies list`, () => {
+    const mockMoviesStore = {
+      [NameSpace.Movies]: {
+        ...initialState,
+        itemsIds: [`1`, `2`, `3`],
+        myListMoviesIds: [`1`, `3`],
+        items: {
+          [`1`]: {...movieCard, id: `1`},
+          [`2`]: {...movieCard, id: `2`},
+          [`3`]: {...movieCard, id: `3`},
+        },
+      },
+    };
+
+    expect(getMyListMovies(mockMoviesStore))
+      .toEqual([
+        {...movieCard, id: `1`},
+        {...movieCard, id: `3`},
+      ]);
   });
 });
