def3ca53662ee5a4466b599c71a661e73955d5d0
Nikolai Karastelev <n.karastelev@credinform.ru>
2019-06-21 16:49:41 +0300

    Update movies selectors state

--- src/reducer/movies/selectors.test.js
+++ src/reducer/movies/selectors.test.js
@@ -1,27 +1,30 @@
-import {ALL_GENRES_GROUP, getMoviesByGenres} from "./selectors";
 import NameSpace from "../name-spaces";
+import movieCard from "../../mocks/movie-card";
+import {getMovies, getMoviesItems, getMovieById} from "./selectors";
+import {initialState} from "./movies";
+
+const mockMovies = {
+  ...initialState,
+  itemsIds: [movieCard.id],
+  items: {
+    [movieCard.id]: movieCard,
+  },
+};
+const mockStore = {
+  [NameSpace.Movies]: mockMovies,
+};
 
 describe(`Movies Selectors`, () => {
-  it(`should return grouped movies by getMoviesByGenres`, () => {
-    const items = [
-      {id: `0`, genre: `1`},
-      {id: `1`, genre: `2`},
-      {id: `2`, genre: `3`},
-      {id: `3`, genre: `2`},
-      {id: `4`, genre: `4`},
-      {id: `5`, genre: `1`},
-      {id: `6`, genre: `5`},
-    ];
-    const state = {[NameSpace.Movies]: {items}};
-    const groups = getMoviesByGenres(state);
-
-    expect(groups).toEqual({
-      [ALL_GENRES_GROUP]: items,
-      1: [items[0], items[5]],
-      2: [items[1], items[3]],
-      3: [items[2]],
-      4: [items[4]],
-      5: [items[6]],
-    });
+  it(`should return movies state`, () => {
+    expect(getMovies(mockStore))
+      .toEqual(mockMovies);
+  });
+  it(`should return movies items object state`, () => {
+    expect(getMoviesItems(mockStore))
+      .toEqual(mockMovies.items);
+  });
+  it(`should return movie by id`, () => {
+    expect(getMovieById(mockStore, movieCard.id))
+      .toEqual(movieCard);
   });
 });
