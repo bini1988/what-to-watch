fc180079ba38ca3f6592c6be3e11029c6825a5e6
Nikolai <rus.nick@mail.ru>
2019-06-23 19:24:59 +0300

    Add react-notifications package

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,6 +2,7 @@ import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
+import {NotificationContainer} from "react-notifications";
 
 import {Operation} from "../../reducer/user/user";
 import withRouteAuth from "../../hocs/with-route-auth";
@@ -43,6 +44,7 @@ class App extends PureComponent {
           <Route
             render={() => <Redirect to="/"/>}/>
         </Switch>
+        <NotificationContainer/>
       </BrowserRouter>
     );
   }
