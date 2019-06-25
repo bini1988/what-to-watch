20405fe5c4e5035ee70332e516e2f83699f1c285
Nikolai <rus.nick@mail.ru>
2019-06-25 22:57:18 +0300

    Add AddReviewPage test

---
+++ src/components/add-review-page/add-review-page.e2e.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import {AddReviewPage} from "./add-review-page";
+import AddReview from "../add-review/add-review";
+
+configure({adapter: new Adapter()});
+
+describe(`ListButton`, () => {
+  it(`Should call onReviewSubmit handler`, () => {
+    const review = {comment: `comment`, rating: 5};
+    const id = 7;
+    const handlePush = jest.fn();
+    const handleOnReviewSubmit = jest.fn(() => Promise.resolve());
+    const wrapper = shallow(
+        <AddReviewPage
+          movie={{id}}
+          history={{push: handlePush}}
+          onReviewSubmit={handleOnReviewSubmit}/>
+    );
+
+    const component = wrapper.find(AddReview);
+
+    return component.prop(`onSubmit`)(review)
+      .then(() => {
+        expect(handleOnReviewSubmit).toHaveBeenCalledWith(id, review);
+        expect(handlePush).toHaveBeenCalledWith(`/film/${id}#Reviews`);
+      });
+  });
+});
