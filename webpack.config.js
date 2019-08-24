0b745d3326f15bd3f370105e21ec9b3709d92b32
Nikolai <rus.nick@mail.ru>
2019-08-24 21:20:32 +0300

    Config loadable  & packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require(`path`);
 
 module.exports = {
-  entry: `./src/index.js`,
+  entry: {
+    index: `./src/index.js`,
+  },
   output: {
     filename: `bundle.js`,
+    chunkFilename: `[name].bundle.js`,
     path: path.join(__dirname, `public`),
     publicPath: `/`,
   },
