74dcca40be605e262e85ce1a905ae8d46f9af13d
Nikolai <rus.nick@mail.ru>
2019-08-24 22:04:04 +0300

    Deploy project for gh-pages

--- src/models/movie-review.test.js
+++
@@ -1,9 +0,0 @@
-import MovieReview from "./movie-review";
-import review, {MovieReviewData} from "../mocks/review";
-
-describe(`MovieReview model`, () => {
-  it(`should return User model`, () => {
-    expect(new MovieReview(MovieReviewData))
-      .toEqual(review);
-  });
-});
--- src/models/user.test.js
+++
@@ -1,9 +0,0 @@
-import User from "./user";
-import user, {UserData} from "../mocks/user";
-
-describe(`User model`, () => {
-  it(`should return User model`, () => {
-    expect(new User(UserData))
-      .toEqual(user);
-  });
-});
