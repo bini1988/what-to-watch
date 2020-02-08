eb71344a690b941596ae7e47466bdf510c5bf2b1
Nikolai <rus.nick@mail.ru>
2020-02-08 17:02:55 +0300

    Config cypress baseUrl param

--- cypress/integration/main-page/sing-in.spec.js
+++ cypress/integration/main-page/sing-in.spec.js
@@ -1,6 +1,6 @@
 describe('Sign in on main page', function () {
   it('Should redirect to sign in page', function () {
-    cy.visit('http://localhost:1337')
+    cy.visit('/')
     cy.contains('Sign in').click()
     cy.url().should('include', '/login')
   });
